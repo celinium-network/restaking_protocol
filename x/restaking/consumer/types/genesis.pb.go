610755a6283fbe775a6b89d885ef77e5510d0d6f
jerrybaoo <shirui_yj133@163.com>
2023-06-19 11:32:24 +0800

    add comment for coordinator protocol file

--- x/restaking/consumer/types/genesis.pb.go
+++ x/restaking/consumer/types/genesis.pb.go
@@ -22,6 +22,7 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 
+// GenesisState defines the restaking consumer module's genesis state.
 type GenesisState struct {
 }
 
